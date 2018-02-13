a7b538cae3fabe1e79a7a50d33c70570206c17a6
Miguel de Barros <miguel@debarros.me>
2018-02-13 21:04:11 +0200

    Feature/#89#69 automate the ci&cd pipeline for forensic logging sidecar (#5)
    * /#89#69AutomateTheCI&CDPipelineForForensic-logging-sidecar:
    
    Added initial config for CI/CD capabilities
    
    * Fixed one change in the circleci config for sonarqube dir
    
    * Integration Testing scripts have been updated for CircleCI v2.0
    
    * Updated docker file to remove .nvmrc
    
    * Updated correct helm set values

--- test.Dockerfile
+++ test.Dockerfile
@@ -2,7 +2,9 @@ FROM mhart/alpine-node:6.5.0
 USER root
 
 WORKDIR /opt/sidecar
+# COPY . /opt/sidecar
 COPY src /opt/sidecar/src
+COPY test /opt/sidecar/test
 COPY migrations /opt/sidecar/migrations
 COPY config /opt/sidecar/config
 COPY package.json server.sh /opt/sidecar/
@@ -10,8 +12,10 @@ COPY package.json server.sh /opt/sidecar/
 RUN apk add --no-cache make gcc g++ python && \
     apk add -U iproute2 && ln -s /usr/lib/tc /lib/tc && \
     apk add -U iptables && \
-    chmod +x /opt/sidecar/server.sh &&\
-    npm install --production
+    chmod +x /opt/sidecar/server.sh && \
+    npm install && \
+    npm install -g tape && \
+    npm install -g tap-xunit
 
 EXPOSE 5678
 
--- test/integration-runner.sh
+++ test/integration-runner.sh
@@ -1,63 +1,157 @@
 #!/bin/bash
-POSTGRES_USER=${POSTGRES_USER:-postgres}
-POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-postgres}
-POSTGRES_HOST=${HOST_IP:-localhost}
-INTG_TEST_CMD=${INTG_TEST_CMD:-tape \'test/integration/**/*.test.js\' | faucet}
-env_file=$1
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 env-file"
-    exit 1
-fi
+>&2 echo "--==== Integration Tests Runner ====--"
 
-psql() {
+if [ $# -ne 1 ]; then 
+    echo ""
+    echo "Usage: $0 {env-file}" 
+    echo "{env-file} must contain the following variables:"
+    echo " - DOCKER_IMAGE: Name of Image"
+    echo " - DOCKER_TAG: Tag/Version of Image"
+    echo " - DOCKER_FILE: Recipe to be used for Docker build"
+    echo " - DOCKER_WORKING_DIR: Docker working directory"
+    echo " - POSTGRES_USER: Posgres user"
+    echo " - POSTGRES_PASSWORD: Posgres password"
+    echo " - POSTGRES_HOST: Posgres host name"
+    echo " - POSTGRES_PORT: Posgres container port"
+    echo " - POSTGRES_DB: Posgres database"
+    echo " - POSTGRES_IMAGE: Docker Image for Posgres"
+    echo " - POSTGRES_TAG: Docker tag/version for Posgres"
+    echo " - APP_HOST: Application host name"
+    echo " - APP_DIR_TEST_RESULTS: Location of test results relative to the working directory"
+    echo " - TEST_CMD: Interation test command to be executed"
+    echo ""
+    echo " * IMPORTANT: Ensure you have the required env in the test/.env to execute the application"
+    echo ""
+    exit 1 
+fi 
+>&2 echo ""
+>&2 echo "====== Loading environment variables ======"
+cat $1
+. $1
+>&2 echo "==========================================="
+>&2 echo ""
+
+>&2 echo "Executing Integration Tests for $APP_HOST ..."
+
+>&2 echo "Creating local directory to store test results"
+mkdir -p test/results
+
+fpsql() {
 	docker run --rm -i \
 		--entrypoint psql \
-    --link postgres-int \
+    --link $POSTGRES_HOST \
+    -e PGUSER=$POSTGRES_USER \
 		-e PGPASSWORD=$POSTGRES_PASSWORD \
-		"postgres:9.4" \
-    --host postgres-int \
+    -e PGDATABASE=$POSTGRES_DB \
+    -e POSTGRES_DB=$POSTGRES_DBNAME \
+		"$POSTGRES_IMAGE:$POSTGRES_TAG" \
+    --host $POSTGRES_HOST \
 		--username $POSTGRES_USER \
-    --dbname postgres \
+    --dbname $POSTGRES_DB \
 		--quiet --no-align --tuples-only \
 		"$@"
 }
 
+ftest() {
+	docker run --rm -i \
+    --link $POSTGRES_HOST \
+    --env POSTGRES_USER="$POSTGRES_USER" \
+    --env POSTGRES_PASSWORD="$POSTGRES_PASSWORD" \
+    --env POSTGRES_HOST="$POSTGRES_HOST" \
+    --env POSTGRES_PORT="$POSTGRES_PORT" \
+    --env POSTGRES_DB="$POSTGRES_DB" \
+		"$DOCKER_IMAGE:$DOCKER_TAG" \
+    /bin/sh \
+    -c \
+    "$@"
+}
+
 is_psql_up() {
-    psql -c '\l' > /dev/null 2>&1
+    fpsql -c '\l' > /dev/null 2>&1
 }
 
 stop_docker() {
-  (docker stop postgres-int && docker rm postgres-int) > /dev/null 2>&1
+  >&2 echo "Posgres-int is shutting down $POSTGRES_HOST"
+  (docker stop $POSTGRES_HOST && docker rm $POSTGRES_HOST) > /dev/null 2>&1
+  >&2 echo "$APP_HOST environment is shutting down"
+  (docker stop $APP_HOST && docker rm $APP_HOST) > /dev/null 2>&1
+}
+
+clean_docker() {
+  stop_docker
+  >&2 echo "Removing docker test image $DOCKER_IMAGE:$DOCKER_TAG"
+  (docker rmi $DOCKER_IMAGE:$DOCKER_TAG) > /dev/null 2>&1
 }
 
 run_test_command()
 {
-  eval "$INTG_TEST_CMD"
+  >&2 echo "Running $APP_HOST Test command: $TEST_CMD"
+  docker run -i \
+    --link $POSTGRES_HOST \
+    --name $APP_HOST \
+    --env POSTGRES_USER="$POSTGRES_USER" \
+    --env POSTGRES_PASSWORD="$POSTGRES_PASSWORD" \
+    --env POSTGRES_HOST="$POSTGRES_HOST" \
+    --env POSTGRES_PORT="$POSTGRES_PORT" \
+    --env POSTGRES_DB="$POSTGRES_DB" \
+		$DOCKER_IMAGE:$DOCKER_TAG \
+    /bin/sh \
+    -c "source test/.env; $TEST_CMD"
 }
 
->&2 echo "Loading environment variables"
-source $env_file
+>&2 echo "Building Docker Image $DOCKER_IMAGE:$DOCKER_TAG with $DOCKER_FILE"
+docker build --no-cache -t $DOCKER_IMAGE:$DOCKER_TAG -f $DOCKER_FILE .
+echo "result "$?""
+if [ "$?" != 0 ]
+then
+  >&2 echo "Build failed...exiting"
+  clean_docker
+  exit 1
+fi
 
 >&2 echo "Postgres is starting"
 stop_docker
-docker run --name postgres-int -d -p 15432:5432 -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -e POSTGRES_USER=$POSTGRES_USER "postgres:9.4" > /dev/null 2>&1
+docker run --name $POSTGRES_HOST -d -p $POSTGRES_PORT:$POSTGRES_PORT -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -e POSTGRES_USER=$POSTGRES_USER -e POSTGRES_DB=$POSTGRES_DB "$POSTGRES_IMAGE:$POSTGRES_TAG" > /dev/null 2>&1
+
+if [ "$?" != 0 ]
+then
+  >&2 echo "Starting Postgres failed...exiting"
+  clean_docker
+  exit 1
+fi
 
 until is_psql_up; do
-  >&2 echo "Postgres is unavailable - sleeping"
+  >&2 echo "Postgres is unavailable...sleeping"
   sleep 1
 done
 
->&2 echo "Postgres is up - creating integration database"
-psql <<'EOSQL'
-    DROP DATABASE IF EXISTS "sidecar_integration";
-	  CREATE DATABASE "sidecar_integration";
-EOSQL
-
-export SIDE_DATABASE_URI="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:15432/sidecar_integration"
-
 >&2 echo "Running migrations"
-npm run migrate > /dev/null 2>&1
+ftest "source test/.env; npm run migrate"
+
+if [ "$?" != 0 ]
+then
+  >&2 echo "Migration failed...exiting"
+  clean_docker
+  exit 1
+fi
 
 >&2 echo "Integration tests are starting"
-set -o pipefail && run_test_command
+run_test_command
+test_exit_code=$?
+
+if [ "$test_exit_code" != 0 ]
+then
+  >&2 echo "Integration tests failed...exiting"
+  >&2 echo "Test environment logs..."
+  docker logs $APP_HOST
+  clean_docker
+  exit 1
+fi
+
+>&2 echo "Copy results to local directory"
+docker cp $APP_HOST:$DOCKER_WORKING_DIR/$APP_DIR_TEST_RESULTS test
+
+clean_docker
+
+exit "$test_exit_code"
